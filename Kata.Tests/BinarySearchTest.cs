867e2dbd6ce275bd0b3d49c83868275b9ffb93d8
Oleg Babenko <majerkravchuk@gmail.com>
2025-07-11 14:09:16 +0200

    Use Theory for set of tests

--- Kata.Tests/BinarySearchTest.cs
+++ Kata.Tests/BinarySearchTest.cs
@@ -3,13 +3,15 @@ using Kata.Exercises;
 namespace Kata.Tests;
 
 public class BinarySearchTest {
-    [Fact]
-    public void TestIndexOf() {
-        var array = new[] { 1, 3, 4, 69, 71, 81, 90, 99, 420, 1337, 69420 };
+    private readonly int[] _array = [1, 3, 4, 69, 71, 81, 90, 99, 420, 1337, 69420];
 
-        Assert.Equal(3, BinarySearch.IndexOf(array, 69));
-        Assert.Equal(-1, BinarySearch.IndexOf(array, 1336));
-        Assert.Equal(10, BinarySearch.IndexOf(array, 69420));
-        Assert.Equal(-1, BinarySearch.IndexOf(array, 69421));
+    [Theory]
+    [InlineData(69, 3)]
+    [InlineData(1336, -1)]
+    [InlineData(69420, 10)]
+    [InlineData(69421, -1)]
+    [InlineData(81, 5)]
+    public void TestIndexOf(int value, int index) {
+        Assert.Equal(index, BinarySearch.IndexOf(_array, value));
     }
 }
