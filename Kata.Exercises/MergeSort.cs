454bd177eef7824bb8101dc89dbaf5d1a8e0dbb9
Oleg Babenko <majerkravchuk@gmail.com>
2025-07-15 12:55:16 +0200

    Add comment for merge sort

--- Kata.Exercises/MergeSort.cs
+++ Kata.Exercises/MergeSort.cs
@@ -1,5 +1,14 @@
 namespace Kata.Exercises;
 
+/**
+ * merge(l, m, h);
+ *
+ * [3, 9, 4, 7] array
+ * [0  1][2  3] indexes
+ *  l  m     h  passed to merge()
+ *  i     j     internal pointers
+ */
+
 public class MergeSort {
     public static void Sort<T>(T[] a) where T : IComparable<T> {
         throw new NotImplementedException();
